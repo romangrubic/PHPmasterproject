dafaaafd3ee2316e9ffb298aa595247107844210
Roman Grubic <grubic.roman@gmail.com>
2021-11-23 15:32:25 +0100

    Translated table to english and removed table 'Adress'
    Changed the names of tables and attributes from croatian to english and moved table 'Adres' into 'Customer' table.

--- database.sql
+++ database.sql
@@ -1,64 +1,54 @@
-drop database if exists trgovina;
-create database trgovina character set utf8;
-use trgovina;
+drop database if exists shop;
+create database shop character set utf8;
+use shop;
 
-create table osoba(
-    sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
+create table customer(
+    id int not null primary key auto_increment,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
     email varchar(255) not null,
-    brojtelefona varchar(15),
-    adresa int
+    phonenumber varchar(15),
+    street varchar(255),
+    city varchar(50),
+    postalnumber char(5)
 );
 
-create table adresa(
-    sifra int not null primary key auto_increment,
-    ulica varchar(255),
-    kucnibroj varchar(10),
-    grad varchar(50),
-    postanskibroj char(5)
+create table category(
+    id int not null primary key auto_increment,
+    name varchar(50) not null,
+    description varchar(255)
 );
 
-create table kategorija(
-    sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    opis varchar(255)
+create table subcategory(
+    id int not null primary key auto_increment,
+    category int not null,
+    name varchar(50) not null,
+    description varchar(255)
 );
 
-create table podkategorija(
-    sifra int not null primary key auto_increment,
-    kategorija int not null,
-    naziv varchar(50) not null,
-    opis varchar(255)
+create table product(
+    id int not null primary key auto_increment,
+    name varchar(255) not null,
+    description text,
+    category int not null,
+    subcategory int not null,
+    price decimal(18,2)
 );
 
-create table proizvod(
-    sifra int not null primary key auto_increment,
-    naziv varchar(255) not null,
-    opis text,
-    kategorija int not null,
-    podkategorija int not null,
-    cijena decimal(18,2),
-    osoba int not null
-);
-
-create table recenzija(
-    sifra int not null primary key auto_increment,
-    osoba int not null,
-    proizvod int not null,
-    rejting int(1) not null,
-    tekst varchar(255) not null, 
-    datum datetime
+create table review(
+    id int not null primary key auto_increment,
+    customer int not null,
+    product int not null,
+    rating int(1) not null,
+    description varchar(255) not null, 
+    date datetime
 );
 
 
-alter table osoba add foreign key (adresa) references adresa(sifra);
+alter table subcategory add foreign key (category) references category(id);
 
-alter table podkategorija add foreign key (kategorija) references kategorija(sifra);
+alter table product add foreign key (category) references category(id);
+alter table product add foreign key (subcategory) references subcategory(id);
 
-alter table proizvod add foreign key (kategorija) references kategorija(sifra);
-alter table proizvod add foreign key (podkategorija) references podkategorija(sifra);
-alter table proizvod add foreign key (osoba) references osoba(sifra);
-
-alter table recenzija add foreign key (osoba) references osoba(sifra);
-alter table recenzija add foreign key (proizvod) references proizvod(sifra);
+alter table review add foreign key (customer) references customer(id);
+alter table review add foreign key (product) references product(id);
