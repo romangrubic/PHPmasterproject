ab50cb3bb6f7fd387ab4b36bca71075a1c9fc2ed
Roman Grubic <grubic.roman@gmail.com>
2021-12-15 22:27:40 +0100

    Creating customercreation procedure.
    Procedure for creation of mock customers. Nested loop with combination of email function and randomcity function.

--- generator.sql
+++ generator.sql
@@ -99,3 +99,59 @@ $$
 DELIMITER ;
 
 
+------------------------------------------------------------------------
+-- Procedura: za svako ime pravimo petlju da dobije svako prezime iz tablice s prezimenima (jedno ime s deset razlicith prezimena),
+--  koristimo emailfunction za njihov email i randomcity da im dodijelimo grad
+
+drop procedure if exists customercreation;
+DELIMITER $$
+create procedure customercreation()
+begin
+
+    declare _firstname varchar(20);
+    declare _lastname varchar(20);
+
+    declare firstname_kraj int default 0;
+
+    declare firstname_cursor cursor for select firstname from firstname order by id;    
+    declare continue handler for not found set firstname_kraj = 1;
+    
+
+    open firstname_cursor;
+
+    firstloop: loop
+
+        fetch firstname_cursor into _firstname;
+
+        if firstname_kraj=1 then leave firstloop;
+        end if;
+
+
+        BLOCK1: begin
+        declare lastname_kraj int default 0;
+        declare lastname_cursor cursor for select lastname from lastname order by id;
+        declare continue handler for not found set lastname_kraj = 1;
+
+        open lastname_cursor;
+
+        secondloop: loop
+
+            fetch lastname_cursor into _lastname;
+
+            if lastname_kraj=1 then leave secondloop;
+            end if;
+
+            insert into customer(id,firstname,lastname,email,city,datecreated) values
+            (null,_firstname,_lastname,emailfunction(_firstname,_lastname),randomcity(),now());
+
+        end loop secondloop;
+        set lastname_kraj=0;
+        end BLOCK1;        
+    
+    end loop firstloop;
+
+end;
+$$
+DELIMITER ;
+
+call customercreation();
